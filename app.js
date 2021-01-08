c593640329335e3e04be965ff145abf7a00cf0d8
Ismael <ismael.quiacu@yahoo.fr>
2021-01-08 10:42:13 +0100

    make it better

--- app.js
+++ app.js
@@ -25,7 +25,9 @@ app.use(express.static(path.join(__dirname, 'reactapp/build')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.get('*', function(req, res) {
+res.sendFile(path.join(__dirname, 'reactapp/build', 'index.html'));
+});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
